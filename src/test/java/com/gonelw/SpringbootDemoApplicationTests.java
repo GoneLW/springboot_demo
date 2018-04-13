44fae8da086c5bbc81fdb56ea5cef58e8f85b6fd
GoneLW <1090253281@qq.com>
2018-04-13 15:12:35 +0800

    test idea commit

--- src/test/java/com/gonelw/SpringbootDemoApplicationTests.java
+++ src/test/java/com/gonelw/SpringbootDemoApplicationTests.java
@@ -13,7 +13,7 @@ public class SpringbootDemoApplicationTests {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "111hello world";
+		return "111he222llo world";
 	}
 
 	public static void main(String[] args) throws Exception {
